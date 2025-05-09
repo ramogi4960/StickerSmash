0027ee7d7184bb5f115ff63820a365c7819b1e53
Kevin <kramzyvine@icloud.com>
2025-05-09 22:17:07 +0300

    Initial Commit

---
+++ StickerSmash/StickerSmashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerSmashApp.swift
+//  StickerSmash
+//
+//  Created by Kevin Ramogi Obuya on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct StickerSmashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StickerSmashTests/StickerSmashTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickerSmashTests.swift
+//  StickerSmashTests
+//
+//  Created by Kevin Ramogi Obuya on 09/05/2025.
+//
+
+import Testing
+@testable import StickerSmash
+
+struct StickerSmashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
